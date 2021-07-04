c4e03c72b555bb19b45e3948611b11e09439c63d
haconehack <86798246+haconehack@users.noreply.github.com>
2021-07-04 09:23:44 +0900

    Update execve_test.c

--- execve_test.c
+++ execve_test.c
@@ -1,8 +1,26 @@
+/*
+        root@ubuntu:~/dev# gcc execve_test.c -o execve_test
+        root@ubuntu:~/dev#
+*/
 #include <stdio.h>
 
 int main()
 {
         char *argd[3] = { "/bin/ls", NULL, NULL };
-        execve("/bin/ls", argd, NULL);
+        char *argdd[] = { "/bin/ls", "-l", NULL };
+        int stu = 0;
+        int apid = 0;
+
+        apid = fork();
+
+        printf("----noptrix noptrix--parent id = 0 and other is above 0\n");
+        if(apid == 0)
+                execve("/bin/ls", argd, NULL);
+
+        printf("---- i'm nop(kj x90 a.k.a vocalist no hacker but do hacks!\n");
+        stu = execvp("ls", argdd);
+
+        if(stu == -1)
+                exit(1);
 
 }
